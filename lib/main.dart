5ec48852561bab1dcf596f2354a8f0b17236f70e
Darshan-Pipaliya1208 <darshanpipaliya999@gmail.com>
2025-05-16 01:48:25 +0530

    First

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:calculator/calculator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CalculatorApp(),
+  ));
+}
